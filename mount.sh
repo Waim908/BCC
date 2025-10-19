ae93a2872c9052df0cc791635c48b4f9f6fefceb
Waim908 <waimxj449@foxmail.com>
2025-10-19 23:35:09 +0800

    fix mount

--- mount.sh
+++ mount.sh
@@ -4,12 +4,10 @@ case $1 in
     sudo mount --bind /proc $2/proc
     sudo mount --bind /sys $2/sys
     sudo mount --bind /dev $2/dev
-    sudo mount --bind /dev/pts $2/dev/pts
   ;;
   unmount)
-    sudo umount $2/dev/pts
-    sudo umount $2/dev
-    sudo umount $2/sys
-    sudo umount $2/proc
+    sudo umount -l $2/dev
+    sudo umount -l $2/sys
+    sudo umount -l $2/proc
   ;;
 esac
